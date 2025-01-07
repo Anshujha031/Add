6988806ee5bc9329bf7204f5760349383c80c12c
naveenjha031 <naveenjha344@gmail.com>
2025-01-07 10:42:01 +0530

    Calculator

---
+++ Calculator.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+
+class Calculator{
+  public static void main(String[] args)
+{
+  Scanner cal = new Scanner(System.in);
+  System.out.println("Tell me the numbers");
+  int num1 = scanner.nextLine();
+  int num2 = scanner.nextLine();
+
+   System.out.println(num1+num2);
+   System.out.println(num1-num2);
+   System.out.println(num1*num2);
+   System.out.println(num1/num2);
+}
+}
+
+
+
+
+   
